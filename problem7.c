524948cf00b1f2c46951ba6fcaeb15c3b09460e8
mahwiz <151815292+herwiz@users.noreply.github.com>
2024-06-09 14:13:40 +0500

    Create problem7.c

---
+++ problem7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// print your name 5 times in while loop
+
+int main() {
+    int count = 1; 
+    while (count <= 5) {
+        printf("Mahwiz\n"); 
+        count++;
+    }
+
+    return 0;
+}
