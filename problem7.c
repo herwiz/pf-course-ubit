c54ea11dbfcc9aedd2be21d1e760fbedf29eaf01
mahwiz <151815292+herwiz@users.noreply.github.com>
2024-06-09 14:15:40 +0500

    Update problem7.c

--- problem7.c
+++ problem7.c
@@ -3,10 +3,16 @@
 // print your name 5 times in while loop
 
 int main() {
-    int count = 1; 
+    int count = 1;
+    int num_times; 
+    char name[100]; // Assuming name length won't exceed 100 characters
+
+    printf("Enter your name: ");
+    scanf("%s", name); // Read the name from the user
+
     while (count <= 5) {
-        printf("Mahwiz\n"); 
-        count++;
+        printf("%s\n", name); // Print the name 
+        count++; 
     }
 
     return 0;
