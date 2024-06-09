8aaf4a2c57e475be9e67c15333d26183e7e5054f
mahwiz <151815292+herwiz@users.noreply.github.com>
2024-06-09 14:19:17 +0500

    Create problem8.c

---
+++ problem8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Create a program that uses a do-while loop to print name "Mahwiz" multiple times. The program should prompt the user to enter 'y' to print again or 'n' to stop.
+
+int main() {
+    char choice;
+    do {
+        printf("Mahwiz\n"); 
+        printf("Do you want to print again? (y/n): ");
+        scanf(" %c", &choice); // Note the space before %c
+    } while (choice == 'y'); // Continue loop if choice is 'y'
+
+    return 0;
+}
